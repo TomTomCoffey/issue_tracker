a6b8d5618ce254848d5a2a33e1de0cae4106e712
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-02-01 22:15:17 -0500

    New issues can be logged into database

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,16 +3,45 @@ import { Button, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface Issue {
+  title: string;
+  description: string;
+}
 
 const newIssue = () => {
+  const { register, control, handleSubmit } = useForm<Issue>();
+  const router = useRouter();
   return (
-    <div className="max-w-xl space-y-4">
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit((data) => {
+        fetch("/api/issues", {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then(() => {
+          router.push("/issues");
+        });
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Type here" />
+        <TextField.Input placeholder="Type here" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="description"></SimpleMDE>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="description" {...field}></SimpleMDE>
+        )}
+      />
+
       <Button>Submit</Button>
-    </div>
+    </form>
   );
 };
 
