0832324872c23321654bdc3f663481904abbacdb
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-02-05 22:57:20 -0500

    Deleting issues now fully functional

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -27,3 +27,21 @@ export async function PATCH(
   });
   return NextResponse.json({ message: "Issue updated" }, { status: 200 });
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) },
+  });
+  return NextResponse.json({ message: "Issue deleted" }, { status: 200 });
+}
--- app/issues/[id]/edit/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/DeleteIssueButton.tsx
@@ -1,10 +1,47 @@
+"use client";
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter();
   return (
     <>
-      <Button color="red">Delete Issue</Button>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content style={{ maxWidth: 450 }}>
+          <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue?
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button
+                variant="solid"
+                color="red"
+                onClick={async () => {
+                  fetch(`/api/issues/${issueId}`, {
+                    method: "DELETE",
+                  }).then(() => {
+                    router.push("/issues");
+                    router.refresh();
+                  });
+                }}
+              >
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </>
   );
 };
