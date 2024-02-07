d4200f11f226a5fdb594c7f9b437141bcc7039b9
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-02-07 17:26:10 -0500

    API endpoint for assigning issue to user

--- app/issues/[id]/AssignSelect.tsx
+++ app/issues/[id]/AssignSelect.tsx
@@ -1,8 +1,20 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AssignSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getThemUsers = async () => {
+      const res = await fetch("/api/users");
+      const data: User[] = await res.json();
+      setUsers(data);
+    };
+    getThemUsers();
+  }, []);
+
   return (
     <>
       <Select.Root>
@@ -10,9 +22,11 @@ const AssignSelect = () => {
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="1">One</Select.Item>
-            <Select.Item value="2">Two</Select.Item>
-            <Select.Item value="3">Three</Select.Item>
+            {users.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
           </Select.Group>
         </Select.Content>
       </Select.Root>
